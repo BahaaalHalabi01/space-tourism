37b9a271494d2756071bd70535a9387b0df65185
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-05-08 23:53:03 +0300

    basic setup tailwindcss

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="text-5xl">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    container: {
+      sm: '576px',
+      md: '960px',
+      lg: '1440px',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
