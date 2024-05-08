4057cacc6f620bf58009806a51318677c1b1719c
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-05-09 00:39:52 +0300

    add fonts and new text size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import * as theme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -5,10 +6,25 @@ export default {
     container: {
       sm: '576px',
       md: '960px',
-      lg: '1440px',
-
+      lg: '1440px'
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        '8xl': '9.375rem',
+        '7xl': '6.25rem',
+        '6xl': '3.5rem',
+        '4xl': '2rem',
+        '3xl': '1.75rem'
+      },
+      letterSpacing: {
+        wide: '.146rem',
+        widest: '.3rem'
+      },
+      fontFamily: {
+        sans: ['"Barlow Condensed"', ...theme.fontFamily.sans],
+        serif: ['Bellefair', ...theme.fontFamily.serif]
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
